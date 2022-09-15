9936833bd820ec9533f4c51a38c85d099926b5a2
Mario Lorenz <mario.lorenz@oxid-esales.com>
2022-09-15 17:51:21 +0200

    first integration test

---
+++ tests/Integration/Service/ServiceAvailabilityTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+namespace OxidSolutionCatalysts\Adyen\Tests\Integration\Service;
+
+use OxidEsales\EshopCommunity\Internal\Container\ContainerFactory;
+use OxidSolutionCatalysts\Adyen\Service\AdyenSDKLoader;
+use OxidSolutionCatalysts\Adyen\Service\Context;
+use OxidSolutionCatalysts\Adyen\Service\ModuleSettings;
+use OxidSolutionCatalysts\Adyen\Service\Repository;
+use OxidSolutionCatalysts\Adyen\Service\StaticContents;
+use PHPUnit\Framework\TestCase;
+
+class ServiceAvailabilityTest extends TestCase
+{
+    /**
+     * @dataProvider serviceAvailabilityDataProvider
+     */
+    public function testServiceAvailable($service): void
+    {
+        $container = ContainerFactory::getInstance()->getContainer();
+        $this->assertInstanceOf($service, $container->get($service));
+    }
+
+    public function serviceAvailabilityDataProvider(): array
+    {
+        return [
+            [AdyenSDKLoader::class],
+            [Context::class]
+            [ModuleSettings::class],
+            [Repository::class],
+            [StaticContents::class],
+        ];
+    }
+}
