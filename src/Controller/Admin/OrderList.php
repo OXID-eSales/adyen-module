143fbf2d3e6c10fb4787a3efcb75915e6f7204ce
Mario Lorenz <mario.lorenz@oxid-esales.com>
2022-09-21 09:54:57 +0200

    sceleton for OrderList-Controller

---
+++ src/Controller/Admin/OrderList.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidSolutionCatalysts\Adyen\Controller\Admin;
+
+/**
+ *
+ * @mixin \OxidEsales\Eshop\Application\Controller\Admin\OrderList
+ */
+class OrderList extends OrderList_parent
+{
+
+}
---
+++ tests/Integration/Controller/Admin/OrderListTest.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidSolutionCatalysts\Adyen\Tests\Integration\Controller\Admin;
+
+use OxidEsales\TestingLibrary\UnitTestCase;
+
+final class OrderListTest extends UnitTestCase
+{
+}
