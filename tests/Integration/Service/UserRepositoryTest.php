3f5c3d2b37ca298c15b29d3397bfd97445586c6c
Mario Lorenz <mario.lorenz@oxid-esales.com>
2022-10-06 16:59:59 +0200

    add Test for getCountryId

--- tests/Integration/Service/UserRepositoryTest.php
+++ tests/Integration/Service/UserRepositoryTest.php
@@ -7,6 +7,7 @@
 
 namespace OxidSolutionCatalysts\Adyen\Tests\Integration\Service;
 
+use OxidEsales\Eshop\Application\Model\Address;
 use OxidEsales\Eshop\Application\Model\User;
 use OxidEsales\Eshop\Application\Model\Country;
 use OxidEsales\Eshop\Core\Registry;
@@ -72,6 +73,8 @@ final class UserRepositoryTest extends TestCase
         string $userPassword,
         string $userCountryId
     ): void {
+
+
         $service = $this->getServiceFromContainer(UserRepository::class);
         $this->assertSame((bool)$userPassword, $service->userAccountExists($userName));
     }
@@ -110,6 +113,40 @@ final class UserRepositoryTest extends TestCase
         $this->assertSame($country->oxcountry__oxisoalpha2->value, $service->getUserCountryIso());
     }
 
+    public function testGetCountryId(
+    ): void {
+        $config = Registry::getConfig();
+        $session = Registry::getSession();
+        $service = $this->getServiceFromContainer(UserRepository::class);
+
+        // Case 1) via GlobalParameter delcountryid
+        $config->setGlobalParameter('delcountryid', 'c456');
+        $this->assertSame('c456', $service->getCountryId());
+
+        // Case 2) via DeliveryAddress
+        $config->setGlobalParameter('delcountryid', '');
+        $session->setVariable('deladrid', 'Dummy');
+        $deliveryAddress = oxNew(Address::class);
+        $deliveryAddress->setId('Dummy');
+        $deliveryAddress->assign([
+            'oxaddress__oxcountryid' => 'c789'
+        ]);
+        $deliveryAddress->save();
+        $this->assertSame('c789', $service->getCountryId());
+
+        // Case 3) via User
+        $session->setVariable('deladrid', '');
+        $user = oxNew(User::class);
+        $user->load('456');
+        $session->setUser($user);
+        $this->assertSame('c456', $service->getCountryId());
+
+        // Case 4) via aHomeCountry - a7c40f631fc920687.20179984 OXID-default HomeCountry
+        $user = oxNew(User::class);
+        $session->setUser($user);
+        $this->assertSame('a7c40f631fc920687.20179984', $service->getCountryId());
+    }
+
     public function providerTestUserData(): array
     {
         return [
@@ -138,6 +175,14 @@ final class UserRepositoryTest extends TestCase
             [
                 'c456',
                 'NL'
+            ],
+            [
+                'c789',
+                'DK'
+            ],
+            [
+                'c321',
+                'US'
             ]
         ];
     }
