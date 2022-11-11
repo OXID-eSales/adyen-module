c34ef49ba86af6ae2a60613af8ae9b37daf870f2
Mario Lorenz <mario.lorenz@oxid-esales.com>
2022-11-11 10:48:21 +0100

    add Model AdyenApiPayments

---
+++ src/Model/AdyenAPIPayments.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidSolutionCatalysts\Adyen\Model;
+
+class AdyenAPIPayments
+{
+    protected string $reference;
+
+    protected string $paymentMethod;
+
+    protected string $merchantAccount;
+
+    protected string $returnUrl;
+
+    protected string $currencyName;
+
+    protected string $currencyAmount;
+
+    protected string $applicationName;
+
+    protected string $applicationVersion;
+
+    public function setReference(string $reference): void
+    {
+        $this->reference = $reference;
+    }
+
+    public function setPaymentMethod(string $paymentMethod): void
+    {
+        $this->paymentMethod = $paymentMethod;
+    }
+
+    public function setMerchantAccount(string $merchantAccount): void
+    {
+        $this->merchantAccount = $merchantAccount;
+    }
+
+    public function setReturnUrl(string $returnUrl): void
+    {
+        $this->returnUrl = $returnUrl;
+    }
+
+    public function setCurrencyName(string $currencyName): void
+    {
+        $this->currencyName = $currencyName;
+    }
+
+    public function setCurrencyAmount(string $currencyAmount): void
+    {
+        $this->currencyAmount = $currencyAmount;
+    }
+
+    public function setMerchantApplicationName(string $applicationName): void
+    {
+        $this->applicationName = $applicationName;
+    }
+
+    public function setMerchantApplicationVersion(string $applicationVersion): void
+    {
+        $this->applicationVersion = $applicationVersion;
+    }
+
+    public function getAdyenPaymentsParams(): array
+    {
+        return [
+            'paymentMethod' => $this->paymentMethod,
+            'amount' => [
+                'currency' => $this->currencyName,
+                'value' => $this->currencyAmount,
+            ],
+            'reference' => $this->reference,
+            'returnUrl' => $this->returnUrl,
+            'merchantAccount' => $this->merchantAccount,
+            'applicationInfo' => [
+                'merchantApplication' => [
+                    'name' => $this->applicationName,
+                    'version' => $this->applicationVersion
+                ]
+            ]
+        ];
+    }
+}
---
+++ tests/Unit/Model/AdyenAPIPaymentsTest.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidSolutionCatalysts\Adyen\Tests\Unit\Model;
+
+use OxidEsales\TestingLibrary\UnitTestCase;
+use OxidSolutionCatalysts\Adyen\Model\AdyenAPIPayments;
+
+class AdyenAPIPaymentsTest extends UnitTestCase
+{
+    public function testSetGetAdyenSessionParams(): void
+    {
+        $model = new AdyenAPIPayments();
+        $model->setCurrencyAmount('1000');
+        $model->setCurrencyName('EUR');
+        $model->setMerchantAccount('TestMerchant');
+        $model->setReference('TestReference');
+        $model->setReturnUrl('ReturnUrl');
+        $model->setPaymentMethod('TestPaymentMethods');
+        $model->setMerchantApplicationName('TestMerchantApplicationName');
+        $model->setMerchantApplicationVersion('Testv1.0.0');
+
+        $this->assertSame([
+            'paymentMethod' => 'TestPaymentMethods',
+            'amount' => [
+                'currency' => 'EUR',
+                'value' => '1000',
+            ],
+            'reference' => 'TestReference',
+            'returnUrl' => 'ReturnUrl',
+            'merchantAccount' => 'TestMerchant',
+            'applicationInfo' => [
+                'merchantApplication' => [
+                    'name' => 'TestMerchantApplicationName',
+                    'version' => 'Testv1.0.0'
+                ]
+            ]
+        ], $model->getAdyenPaymentsParams());
+    }
+}
