213a6c9b6b66d039a8299e2ca8a0727c167b675f
Mario Lorenz <mario.lorenz@oxid-esales.com>
2022-11-17 15:28:28 +0100

    add doAdyenCapture

--- src/Service/Payment.php
+++ src/Service/Payment.php
@@ -14,6 +14,7 @@ use OxidEsales\Eshop\Application\Model\Order as eShopOrder;
 use OxidEsales\Eshop\Core\Registry;
 use OxidEsales\Eshop\Core\Session;
 use OxidSolutionCatalysts\Adyen\Core\Module;
+use OxidSolutionCatalysts\Adyen\Model\AdyenAPICaptures;
 use OxidSolutionCatalysts\Adyen\Model\AdyenAPIPayments;
 use OxidSolutionCatalysts\Adyen\Model\Order;
 use OxidSolutionCatalysts\PayPalApi\Exception\ApiException;
@@ -30,6 +31,8 @@ class Payment
 
     private ?array $paymentResult = null;
 
+    private ?array $captureResult = null;
+
     /** @var Session */
     private Session $session;
 
@@ -43,20 +46,25 @@ class Payment
     private ModuleSettings $moduleSettings;
 
     /** @var AdyenAPIResponsePayments */
-    private AdyenAPIResponsePayments $APIResponse;
+    private AdyenAPIResponsePayments $APIPayments;
+
+    /** @var AdyenAPIResponsePayments */
+    private AdyenAPIResponseCaptures $APICaptures;
 
     public function __construct(
         Session $session,
         Context $context,
         UserRepository $userRepository,
         ModuleSettings $moduleSettings,
-        AdyenAPIResponsePayments $APIResponse
+        AdyenAPIResponsePayments $APIPayments,
+        AdyenAPIResponseCaptures $APICaptures
     ) {
         $this->session = $session;
         $this->context = $context;
         $this->userRepository = $userRepository;
         $this->moduleSettings = $moduleSettings;
-        $this->APIResponse = $APIResponse;
+        $this->APIPayments = $APIPayments;
+        $this->APICaptures = $APICaptures;
     }
 
     public function getSessionPaymentId(): string
@@ -87,11 +95,24 @@ class Payment
         return $this->paymentResult;
     }
 
+    public function setCaptureResult(array $captureResult): void
+    {
+        $this->captureResult = $captureResult;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCaptureResult()
+    {
+        return $this->captureResult;
+    }
+
     /**
      * @param double $amount Goods amount
      * @param eShopOrder $order User ordering object
      */
-    public function doAdyenPayment($amount, $order): bool
+    public function doAdyenAuthorization(float $amount, eShopOrder $order): bool
     {
         $result = false;
 
@@ -102,23 +123,18 @@ class Payment
         // not necessary anymore, so cleanup
         $this->session->deleteVariable(Module::ADYEN_SESSION_PAYMENTSTATEDATA_NAME);
 
-        $currencyDecimals = $this->context->getActiveCurrencyDecimals();
-        $decimalFactor = (int)('1' . str_repeat('0', $currencyDecimals));
-        $currencyAmountInt = $amount * $decimalFactor;
-        $currencyAmount = (string)$currencyAmountInt;
-
         $payments = oxNew(AdyenAPIPayments::class);
         $payments->setCurrencyName($this->context->getActiveCurrencyName());
         $payments->setReference($reference);
         $payments->setPaymentMethod($paymentState ?: []);
-        $payments->setCurrencyAmount($currencyAmount);
+        $payments->setCurrencyAmount($this->getAdyenAmount($amount));
         $payments->setMerchantAccount($this->moduleSettings->getMerchantAccount());
         $payments->setReturnUrl($this->context->getPaymentReturnUrl());
         $payments->setMerchantApplicationName(Module::MODULE_NAME_EN);
         $payments->setMerchantApplicationVersion(Module::MODULE_VERSION_FULL);
 
         try {
-            $result = $this->APIResponse->getPayments($payments);
+            $result = $this->APIPayments->getPayments($payments);
             $this->setPaymentResult($result);
             $result = true;
         } catch (Exception $exception) {
@@ -126,5 +142,40 @@ class Payment
             $this->setPaymentExecutionError(self::PAYMENT_ERROR_GENERIC);
         }
         return $result;
+    }
+
+    /**
+     * @param double $amount Goods amount
+     * @param string $pspReference User ordering object
+     * @param string $orderNr as unique reference
+     */
+    public function doAdyenCapture(float $amount, string $pspReference, string $orderNr): bool
+    {
+        $result = false;
+
+        $captures = oxNew(AdyenAPICaptures::class);
+        $captures->setCurrencyName($this->context->getActiveCurrencyName());
+        $captures->setReference($orderNr);
+        $captures->paymentPspReference($pspReference);
+        $captures->setCurrencyAmount($this->getAdyenAmount($amount));
+        $captures->setMerchantAccount($this->moduleSettings->getMerchantAccount());
+        $captures->setMerchantApplicationName(Module::MODULE_NAME_EN);
+        $captures->setMerchantApplicationVersion(Module::MODULE_VERSION_FULL);
+
+        try {
+            $result = $this->APICaptures->setCapture($captures);
+            $this->setCaptureResult($result);
+            $result = true;
+        } catch (Exception $exception) {
+            Registry::getLogger()->error("Error on setCapture call.", [$exception]);
+        }
+        return $result;
+    }
+
+    protected function getAdyenAmount(float $amount): string {
+        $currencyDecimals = $this->context->getActiveCurrencyDecimals();
+        $decimalFactor = (int)('1' . str_repeat('0', $currencyDecimals));
+        $currencyAmountInt = $amount * $decimalFactor;
+        return (string)$currencyAmountInt;
     }
 }
