de90c9dd5cc343170e1f1e357e87bcbc768494e4
Mario Lorenz <mario.lorenz@oxid-esales.com>
2022-12-06 14:01:37 +0100

    Add Webhook for Refund & Cancel

---
+++ src/Core/Webhook/Handler/RefundHandler.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidSolutionCatalysts\Adyen\Core\Webhook\Handler;
+
+use OxidSolutionCatalysts\Adyen\Core\Module;
+use OxidSolutionCatalysts\Adyen\Core\Webhook\Event;
+
+final class RefundHandler extends WebhookHandlerBase
+{
+    public const REFUND_EVENT_CODE = "REFUND";
+
+    /**
+     * @param Event $event
+     * @return void
+     * @SuppressWarnings(PHPMD.StaticAccess)
+     */
+    protected function additionalUpdates(Event $event): void
+    {
+    }
+
+    protected function getAdyenAction(): string
+    {
+        return Module::ADYEN_STATUS_REFUNDED;
+    }
+
+    protected function getAdyenStatus(): string
+    {
+        return Module::ADYEN_ACTION_REFUND;
+    }
+}
