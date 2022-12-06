06eef60562f99d425293760d4a18b9afef2f8841
Mario Lorenz <mario.lorenz@oxid-esales.com>
2022-12-06 13:21:22 +0100

    storno partly an order

--- src/Controller/Admin/OrderArticle.php
+++ src/Controller/Admin/OrderArticle.php
@@ -9,6 +9,7 @@ declare(strict_types=1);
 
 namespace OxidSolutionCatalysts\Adyen\Controller\Admin;
 
+use OxidEsales\Eshop\Application\Model\Order;
 use OxidSolutionCatalysts\Adyen\Traits\ServiceContainer;
 
 /**
@@ -19,8 +20,31 @@ class OrderArticle extends OrderArticle_parent
 {
     use ServiceContainer;
 
+    /**
+     * @inheritDoc
+     */
     public function storno(): void
     {
+        $order = oxNew(Order::class);
+        $orderLoaded = $order->load($this->getEditObjectId());
+        $amountBefore = 0;
+
+        // collect the Order Amount before the change
+        /** @var \OxidSolutionCatalysts\Adyen\Model\Order $order */
+        if ($orderLoaded && $order->isAdyenOrder()) {
+            $amountBefore = (float)$order->getTotalOrderSum();
+        }
+
         parent::storno();
+
+        // load order again because of the changes
+        $orderLoaded = $order->load($this->getEditObjectId());
+
+        // if Refund is possible, collect the Order Amount after the change
+        if ($orderLoaded && $order->isAdyenOrder() && $order->isAdyenRefundPossible()) {
+            $amountAfter = (float)$order->getTotalOrderSum();
+            $amount = $amountBefore - $amountAfter;
+            $order->refundAdyenOrder($amount);
+        }
     }
 }
