0647f14d0ccc5377c5dd145a73c93706d8af75ba
Mario Lorenz <mario.lorenz@oxid-esales.com>
2022-12-21 21:24:09 +0100

    correct comments

--- src/Traits/ServiceContainer.php
+++ src/Traits/ServiceContainer.php
@@ -15,16 +15,9 @@ use OxidEsales\EshopCommunity\Internal\Container\ContainerFactory;
  * Convenience trait to fetch services from DI container.
  * Use for example in classes where it's not possible to inject services in
  * the constructor because constructor is inherited from a shop core class.
- * Example: see module controllers
  */
 trait ServiceContainer
 {
-    /**
-     * @template T
-     * @psalm-param class-string<T> $serviceName
-     *
-     * @return T
-     */
     protected function getServiceFromContainer(string $serviceName)
     {
         return ContainerFactory::getInstance()
