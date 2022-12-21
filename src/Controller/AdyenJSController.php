b921a761a17eac52e1f935aea8bf68d986494e1e
Mario Lorenz <mario.lorenz@oxid-esales.com>
2022-12-21 21:24:55 +0100

    payments-Endpoint for JS

--- src/Controller/AdyenJSController.php
+++ src/Controller/AdyenJSController.php
@@ -3,23 +3,44 @@
 namespace OxidSolutionCatalysts\Adyen\Controller;
 
 use OxidEsales\Eshop\Application\Controller\FrontendController;
+use OxidEsales\Eshop\Application\Model\Basket;
+use OxidEsales\Eshop\Core\Registry;
+use OxidSolutionCatalysts\Adyen\Service\Payment;
 use OxidSolutionCatalysts\Adyen\Service\ResponseHandler;
+use OxidSolutionCatalysts\Adyen\Service\SessionSettings;
 use OxidSolutionCatalysts\Adyen\Traits\AdyenAPI;
+use OxidSolutionCatalysts\Adyen\Traits\Json;
 
 class AdyenJSController extends FrontendController
 {
     use AdyenAPI;
+    use Json;
 
     /**
-     * @throws \Adyen\AdyenException
      * @throws \JsonException
      */
     public function payments(): void
     {
         $response = $this->getServiceFromContainer(ResponseHandler::class)->response();
+        $session = $this->getServiceFromContainer(SessionSettings::class);
+        $reference = $session->createOrderReference();
+
+        /** @var Basket $basket */
+        $basket = Registry::getSession()->getBasket();
+        $amount = $basket->getPrice()->getBruttoPrice();
+        $paymentMethod = $this->jsonToArray($this->getJsonPostData());
+
+        if (!$amount || !isset($paymentMethod['paymentMethod'])) {
+            $response->setNotFound()->sendJson();
+        }
+
+        /** @var Payment $paymentService */
+        $paymentService = $this->getServiceFromContainer(Payment::class);
+        $paymentService->collectPayments($amount, $reference, $paymentMethod['paymentMethod']['paymentMethod']);
+        $payments = $paymentService->getPaymentResult();
 
         $response->setData(
-            ['WIP']
+            $payments
         )->sendJson();
     }
 }
