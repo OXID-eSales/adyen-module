9643fac4d98f7a1bb0b5763d04986925b2f3d1e2
Mario Lorenz <mario.lorenz@oxid-esales.com>
2023-01-05 11:24:53 +0100

    Dont use Services anymore during install / uninstall

--- src/Core/ModuleEvents.php
+++ src/Core/ModuleEvents.php
@@ -68,18 +68,8 @@ final class ModuleEvents
             }
         }
 
-        /*
-         * We could not yet access the services in the onActivate,
-         * since they are only available after the onActivate. When
-         * deactivating, the services "still" exist when the method
-         * is called (no longer after that). That's why we can still
-         * use them here
-         */
-
         /** @var ModuleSettings $service */
-        $service = ContainerFactory::getInstance()
-            ->getContainer()
-            ->get(ModuleSettings::class);
+        $service = self::getModuleSettingsService();
         $service->saveActivePayments($activePaymentMethods);
     }
 
@@ -111,10 +101,6 @@ final class ModuleEvents
         $service->ensurePaymentMethods();
     }
 
-    /**
-     * @throws ContainerExceptionInterface
-     * @throws NotFoundExceptionInterface
-     */
     private static function getStaticContentService(): StaticContents
     {
         /*
@@ -131,18 +117,38 @@ final class ModuleEvents
         /** @var ContainerInterface $container */
         $container = ContainerFactory::getInstance()
             ->getContainer();
-        /** @var ModuleSettingBridgeInterface $moduleSettingBridge */
-        $moduleSettingBridge = $container->get(ModuleSettingBridgeInterface::class);
         /** @var QueryBuilderFactoryInterface $queryBuilderFactory */
         $queryBuilderFactory = $container->get(QueryBuilderFactoryInterface::class);
 
-        $moduleSettings = new ModuleSettings(
-            $moduleSettingBridge
-        );
+        $moduleSettings = self::getModuleSettingsService();
 
         return new StaticContents(
             $queryBuilderFactory,
             $moduleSettings
+        );
+    }
+
+    private static function getModuleSettingsService(): ModuleSettings
+    {
+        /*
+        Normally I would fetch the ModuleSettings service like this:
+
+        $service = ContainerFactory::getInstance()
+            ->getContainer()
+            ->get(ModuleSettings::class);
+
+        But the services are not ready when the onActivate method is triggered.
+        That's why I build the containers by hand as an exception.:
+        */
+
+        /** @var ContainerInterface $container */
+        $container = ContainerFactory::getInstance()
+            ->getContainer();
+        /** @var ModuleSettingBridgeInterface $moduleSettingBridge */
+        $moduleSettingBridge = $container->get(ModuleSettingBridgeInterface::class);
+
+        return new ModuleSettings(
+            $moduleSettingBridge
         );
     }
 }
