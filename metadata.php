edfc741ac3b0b475e8c64be6ee0709c95f7a62d5
Mario Lorenz <mario.lorenz@oxid-esales.com>
2023-01-06 08:23:25 +0100

    use pspReference Input-Elements twice

--- metadata.php
+++ metadata.php
@@ -80,6 +80,7 @@ $aModule = [
         'modules/osc/adyen/payment/adyen_assets.tpl' => 'osc/adyen/views/frontend/tpl/payment/adyen_assets.tpl',
         'modules/osc/adyen/payment/adyen_payment.tpl' => 'osc/adyen/views/frontend/tpl/payment/adyen_payment.tpl',
         'modules/osc/adyen/payment/adyen_payment_nextstep.tpl' => 'osc/adyen/views/frontend/tpl/payment/adyen_payment_nextstep.tpl',
+        'modules/osc/adyen/payment/adyen_payment_psp.tpl' => 'osc/adyen/views/frontend/tpl/payment/adyen_payment_psp.tpl',
         // frontend - orderpage
         'modules/osc/adyen/payment/adyen_order_submit.tpl' => 'osc/adyen/views/frontend/tpl/payment/adyen_order_submit.tpl',
         // frontend - account
