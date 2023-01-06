0a7e2f2b91d441b5878e05b238fa663c5fe44448
Mario Lorenz <mario.lorenz@oxid-esales.com>
2023-01-06 08:23:00 +0100

    introduce central RequestGetter as Trait

---
+++ src/Traits/RequestGetter.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidSolutionCatalysts\Adyen\Traits;
+
+use OxidEsales\EshopCommunity\Core\Request;
+
+/**
+ * Convenience trait to work with Request-Data
+ */
+trait RequestGetter
+{
+    /**
+     * @SuppressWarnings(PHPMD.StaticAccess)
+     */
+    protected function getStringRequestData(string $key): string
+    {
+        $request = oxNew(Request::class);
+        /** @var string $value */
+        $value = $request->getRequestParameter($key, '');
+        return $value;
+    }
+}
