c4c886290bca31339d1789260dd5eb5473eecd99
René Gust <info@rene-gust.de>
2023-02-07 11:15:09 +0100

    AM-74 refactors loading of .env parameters

---
+++ tests/additional.inc.php
@@ -0,0 +1,8 @@
+<?php
+# this php is included by vendor/oxid-esales/testing-library/library/Bootstrap/UnitBootstrap.php
+
+use Dotenv\Dotenv;
+
+# load .env parameters for the tests
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
