c89bab0b90546a9f8e11466ab61d48e5b9d53f2a
Mario Lorenz <mario.lorenz@oxid-esales.com>
2023-05-04 17:08:55 +0200

    add missing help translation and simply dynamic payment-translations

--- views/admin/de/module_options.php
+++ views/admin/de/module_options.php
@@ -8,25 +8,7 @@
 declare(strict_types=1);
 
 use OxidSolutionCatalysts\Adyen\Core\Module;
-use OxidSolutionCatalysts\Adyen\Model\ModuleOptionsCaptureDelay;
 use OxidSolutionCatalysts\Adyen\Service\ModuleSettings;
-
-$keyDelayCreditCard = 'SHOP_MODULE_osc_adyen_CaptureDelay_' . Module::PAYMENT_CREDITCARD_ID;
-$descCreditCard = Module::PAYMENT_DEFINTIONS[Module::PAYMENT_CREDITCARD_ID]['descriptions']['de']['desc'];
-$keyDelayPayPal = 'SHOP_MODULE_osc_adyen_CaptureDelay_' . Module::PAYMENT_PAYPAL_ID;
-$descPayPal = Module::PAYMENT_DEFINTIONS[Module::PAYMENT_PAYPAL_ID]['descriptions']['de']['desc'];
-$keyDelayGooglePay = 'SHOP_MODULE_osc_adyen_CaptureDelay_' . Module::PAYMENT_GOOGLE_PAY_ID;
-$descGooglePay = Module::PAYMENT_DEFINTIONS[Module::PAYMENT_GOOGLE_PAY_ID]['descriptions']['de']['desc'];
-$keyDelayKlarnaImmediate = 'SHOP_MODULE_osc_adyen_CaptureDelay_' . Module::PAYMENT_KLARNA_IMMEDIATE_ID;
-$descKlarnaImmediate = Module::PAYMENT_DEFINTIONS[Module::PAYMENT_KLARNA_IMMEDIATE_ID]['descriptions']['de']['desc'];
-$keyDelayKlarnaLater = 'SHOP_MODULE_osc_adyen_CaptureDelay_' . Module::PAYMENT_KLARNA_LATER_ID;
-$descKlarnaLater = Module::PAYMENT_DEFINTIONS[Module::PAYMENT_KLARNA_LATER_ID]['descriptions']['de']['desc'];
-$keyDelayKlarnaOverTime = 'SHOP_MODULE_osc_adyen_CaptureDelay_' . Module::PAYMENT_KLARNA_OVER_TIME_ID;
-$descKlarnaOverTime = Module::PAYMENT_DEFINTIONS[Module::PAYMENT_KLARNA_OVER_TIME_ID]['descriptions']['de']['desc'];
-$keyDelayTwint = 'SHOP_MODULE_osc_adyen_CaptureDelay_' . Module::PAYMENT_TWINT_ID;
-$descTwint = Module::PAYMENT_DEFINTIONS[Module::PAYMENT_TWINT_ID]['descriptions']['de']['desc'];
-$keyDelayApplePay = 'SHOP_MODULE_osc_adyen_CaptureDelay_' . Module::PAYMENT_APPLE_PAY_ID;
-$descApplePay = Module::PAYMENT_DEFINTIONS[Module::PAYMENT_APPLE_PAY_ID]['descriptions']['de']['desc'];
 
 $aLang = [
     'charset' => 'UTF-8',
@@ -42,7 +24,9 @@ $aLang = [
     'SHOP_MODULE_' . ModuleSettings::OPERATION_MODE . '_sandbox' => 'Sandbox',
     'SHOP_MODULE_' . ModuleSettings::OPERATION_MODE . '_live' => 'Live',
     'SHOP_MODULE_' . ModuleSettings::LOGGING_ACTIVE => 'Protokollierung aktiv?',
+    'HELP_SHOP_MODULE_' . ModuleSettings::LOGGING_ACTIVE => 'Bei Aktivierung werden alle API-Calls in eine Log-Datei (.../log/adyen/...) geschrieben. Die Informationen sind hilfreich für den Support und das Debugging.',
     'SHOP_MODULE_' . ModuleSettings::ANALYTICS_ACTIVE => 'Analytics aktiv?',
+    'HELP_SHOP_MODULE_' . ModuleSettings::ANALYTICS_ACTIVE => 'Bei Aktivierung werden Analyse-Daten während des Checkouts gesammelt und an Adyen übertragen. Informationen dazu unter: https://docs.adyen.com/online-payments/analytics-and-data-tracking',
 
     'SHOP_MODULE_' . ModuleSettings::SANDBOX_API_KEY => 'API Schlüssel',
     'SHOP_MODULE_' . ModuleSettings::SANDBOX_CLIENT_KEY => 'Client Schlüssel',
@@ -68,37 +52,38 @@ $aLang = [
         'Geben Sie für jede OXID-Sprache (Stammdaten > Sprachen > Sprache > Sprachkürzel) eine passende Sprach- und Regionlokalisierung
         (ISO 639-1 alpha-2 / ISO 3166-1 alpha-2) an. Für jede Einstellung eine Zeile (z.B. en => en_US)',
 ];
-$moduleOptionsCaptureDelay = new ModuleOptionsCaptureDelay();
 
-$aLang = array_merge(
-    $aLang,
-    $moduleOptionsCaptureDelay->getTranslationDEArrayForOption($keyDelayCreditCard, $descCreditCard),
-);
-$aLang = array_merge(
-    $aLang,
-    $moduleOptionsCaptureDelay->getTranslationDEArrayForOption($keyDelayPayPal, $descPayPal),
-);
-$aLang = array_merge(
-    $aLang,
-    $moduleOptionsCaptureDelay->getTranslationDEArrayForOption($keyDelayGooglePay, $descGooglePay),
-);
-$aLang = array_merge(
-    $aLang,
-    $moduleOptionsCaptureDelay->getTranslationDEArrayForOption($keyDelayKlarnaImmediate, $descKlarnaImmediate),
-);
-$aLang = array_merge(
-    $aLang,
-    $moduleOptionsCaptureDelay->getTranslationDEArrayForOption($keyDelayKlarnaLater, $descKlarnaLater),
-);
-$aLang = array_merge(
-    $aLang,
-    $moduleOptionsCaptureDelay->getTranslationDEArrayForOption($keyDelayKlarnaOverTime, $descKlarnaOverTime),
-);
-$aLang = array_merge(
-    $aLang,
-    $moduleOptionsCaptureDelay->getTranslationDEArrayForOption($keyDelayTwint, $descTwint),
-);
-$aLang = array_merge(
-    $aLang,
-    $moduleOptionsCaptureDelay->getTranslationENArrayForOption($keyDelayApplePay, $descApplePay),
-);
+$paymentLang = [
+    ModuleSettings::CAPTURE_DELAY . Module::PAYMENT_CREDITCARD_ID =>
+        Module::PAYMENT_DEFINTIONS[Module::PAYMENT_CREDITCARD_ID]['descriptions']['de']['desc'],
+    ModuleSettings::CAPTURE_DELAY . Module::PAYMENT_PAYPAL_ID =>
+        Module::PAYMENT_DEFINTIONS[Module::PAYMENT_PAYPAL_ID]['descriptions']['de']['desc'],
+    ModuleSettings::CAPTURE_DELAY . Module::PAYMENT_GOOGLE_PAY_ID =>
+        Module::PAYMENT_DEFINTIONS[Module::PAYMENT_GOOGLE_PAY_ID]['descriptions']['de']['desc'],
+    ModuleSettings::CAPTURE_DELAY . Module::PAYMENT_KLARNA_IMMEDIATE_ID =>
+        Module::PAYMENT_DEFINTIONS[Module::PAYMENT_KLARNA_IMMEDIATE_ID]['descriptions']['de']['desc'],
+    ModuleSettings::CAPTURE_DELAY . Module::PAYMENT_KLARNA_LATER_ID =>
+        Module::PAYMENT_DEFINTIONS[Module::PAYMENT_KLARNA_LATER_ID]['descriptions']['de']['desc'],
+    ModuleSettings::CAPTURE_DELAY . Module::PAYMENT_KLARNA_OVER_TIME_ID =>
+        Module::PAYMENT_DEFINTIONS[Module::PAYMENT_KLARNA_OVER_TIME_ID]['descriptions']['de']['desc'],
+    ModuleSettings::CAPTURE_DELAY . Module::PAYMENT_TWINT_ID =>
+        Module::PAYMENT_DEFINTIONS[Module::PAYMENT_TWINT_ID]['descriptions']['de']['desc'],
+    ModuleSettings::CAPTURE_DELAY . Module::PAYMENT_APPLE_PAY_ID =>
+        Module::PAYMENT_DEFINTIONS[Module::PAYMENT_APPLE_PAY_ID]['descriptions']['de']['desc']
+];
+
+foreach ($paymentLang as $key => $description) {
+    $key = 'SHOP_MODULE_' . $key;
+    $paymentLang = [
+        $key => $description,
+        $key . '_' . Module::ADYEN_CAPTURE_DELAY_MANUAL => 'Manuell',
+        $key . '_' . Module::ADYEN_CAPTURE_DELAY_DAYS => 'n Tagen',
+        $key . '_' . Module::ADYEN_CAPTURE_DELAY_IMMEDIATE => 'Sofort',
+        'HELP_' . $key => 'In Adyen kann man die Verzögerung des Geldeinzugs für '
+            . $description . 'definieren: "Immediate", "after n days" oder "Manual".'
+            . 'Die Adyen-Einstellung muss mit der vom Shop korrespondieren.'
+            . ' Im Fall "Manual" kann unter Bestellungen verwalten > Bestellungen > Reiter Adyen,'
+            . ' der Geldeinzug angestoßen werden.'
+    ];
+    $aLang += $paymentLang;
+}
--- views/admin/en/module_options.php
+++ views/admin/en/module_options.php
@@ -8,25 +8,7 @@
 declare(strict_types=1);
 
 use OxidSolutionCatalysts\Adyen\Core\Module;
-use OxidSolutionCatalysts\Adyen\Model\ModuleOptionsCaptureDelay;
 use OxidSolutionCatalysts\Adyen\Service\ModuleSettings;
-
-$keyDelayCreditCard = 'SHOP_MODULE_osc_adyen_CaptureDelay_' . Module::PAYMENT_CREDITCARD_ID;
-$descCreditCard = Module::PAYMENT_DEFINTIONS[Module::PAYMENT_CREDITCARD_ID]['descriptions']['en']['desc'];
-$keyDelayPayPal = 'SHOP_MODULE_osc_adyen_CaptureDelay_' . Module::PAYMENT_PAYPAL_ID;
-$descPayPal = Module::PAYMENT_DEFINTIONS[Module::PAYMENT_PAYPAL_ID]['descriptions']['en']['desc'];
-$keyDelayGooglePay = 'SHOP_MODULE_osc_adyen_CaptureDelay_' . Module::PAYMENT_GOOGLE_PAY_ID;
-$descGooglePay = Module::PAYMENT_DEFINTIONS[Module::PAYMENT_GOOGLE_PAY_ID]['descriptions']['de']['desc'];
-$keyDelayKlarnaImmediate = 'SHOP_MODULE_osc_adyen_CaptureDelay_' . Module::PAYMENT_KLARNA_IMMEDIATE_ID;
-$descKlarnaImmediate = Module::PAYMENT_DEFINTIONS[Module::PAYMENT_KLARNA_IMMEDIATE_ID]['descriptions']['de']['desc'];
-$keyDelayKlarnaLater = 'SHOP_MODULE_osc_adyen_CaptureDelay_' . Module::PAYMENT_KLARNA_LATER_ID;
-$descKlarnaLater = Module::PAYMENT_DEFINTIONS[Module::PAYMENT_KLARNA_LATER_ID]['descriptions']['de']['desc'];
-$keyDelayKlarnaOverTime = 'SHOP_MODULE_osc_adyen_CaptureDelay_' . Module::PAYMENT_KLARNA_OVER_TIME_ID;
-$descKlarnaOverTime = Module::PAYMENT_DEFINTIONS[Module::PAYMENT_KLARNA_OVER_TIME_ID]['descriptions']['de']['desc'];
-$keyDelayTwint = 'SHOP_MODULE_osc_adyen_CaptureDelay_' . Module::PAYMENT_TWINT_ID;
-$descTwint = Module::PAYMENT_DEFINTIONS[Module::PAYMENT_TWINT_ID]['descriptions']['de']['desc'];
-$keyDelayApplePay = 'SHOP_MODULE_osc_adyen_CaptureDelay_' . Module::PAYMENT_APPLE_PAY_ID;
-$descApplePay = Module::PAYMENT_DEFINTIONS[Module::PAYMENT_APPLE_PAY_ID]['descriptions']['de']['desc'];
 
 $aLang = [
     'charset' => 'UTF-8',
@@ -42,7 +24,9 @@ $aLang = [
     'SHOP_MODULE_' . ModuleSettings::OPERATION_MODE . '_sandbox' => 'Sandbox',
     'SHOP_MODULE_' . ModuleSettings::OPERATION_MODE . '_live' => 'Live',
     'SHOP_MODULE_' . ModuleSettings::LOGGING_ACTIVE => 'Logging active?',
+    'HELP_SHOP_MODULE_' . ModuleSettings::LOGGING_ACTIVE => 'If activated, all API calls are written to a log file (.../log/adyen/...). The information is helpful for support and debugging.',
     'SHOP_MODULE_' . ModuleSettings::ANALYTICS_ACTIVE => 'Analytics active?',
+    'HELP_SHOP_MODULE_' . ModuleSettings::ANALYTICS_ACTIVE => 'If activated, analysis data is collected during checkout and transmitted to Adyen. Information on this at: https://docs.adyen.com/online-payments/analytics-and-data-tracking',
 
     'SHOP_MODULE_' . ModuleSettings::SANDBOX_API_KEY => 'API Key',
     'SHOP_MODULE_' . ModuleSettings::SANDBOX_CLIENT_KEY => 'Client Key',
@@ -67,37 +51,38 @@ $aLang = [
         'Enter a suitable language and region localization (ISO 639-1 alpha-2 / ISO 3166-1 alpha-2) for each OXID language
         (Master Settings > Languages > Language > Language abbreviation). One line for each setting (e.g. en => en_US)',
 ];
-$moduleOptionsCaptureDelay = new ModuleOptionsCaptureDelay();
 
-$aLang = array_merge(
-    $aLang,
-    $moduleOptionsCaptureDelay->getTranslationENArrayForOption($keyDelayCreditCard, $descCreditCard),
-);
-$aLang = array_merge(
-    $aLang,
-    $moduleOptionsCaptureDelay->getTranslationENArrayForOption($keyDelayPayPal, $descPayPal),
-);
-$aLang = array_merge(
-    $aLang,
-    $moduleOptionsCaptureDelay->getTranslationENArrayForOption($keyDelayGooglePay, $descGooglePay),
-);
-$aLang = array_merge(
-    $aLang,
-    $moduleOptionsCaptureDelay->getTranslationENArrayForOption($keyDelayKlarnaImmediate, $descKlarnaImmediate),
-);
-$aLang = array_merge(
-    $aLang,
-    $moduleOptionsCaptureDelay->getTranslationENArrayForOption($keyDelayKlarnaLater, $descKlarnaLater),
-);
-$aLang = array_merge(
-    $aLang,
-    $moduleOptionsCaptureDelay->getTranslationENArrayForOption($keyDelayKlarnaOverTime, $descKlarnaOverTime),
-);
-$aLang = array_merge(
-    $aLang,
-    $moduleOptionsCaptureDelay->getTranslationENArrayForOption($keyDelayTwint, $descTwint),
-);
-$aLang = array_merge(
-    $aLang,
-    $moduleOptionsCaptureDelay->getTranslationENArrayForOption($keyDelayApplePay, $descApplePay),
-);
+$paymentLang = [
+    ModuleSettings::CAPTURE_DELAY . Module::PAYMENT_CREDITCARD_ID =>
+        Module::PAYMENT_DEFINTIONS[Module::PAYMENT_CREDITCARD_ID]['descriptions']['en']['desc'],
+    ModuleSettings::CAPTURE_DELAY . Module::PAYMENT_PAYPAL_ID =>
+        Module::PAYMENT_DEFINTIONS[Module::PAYMENT_PAYPAL_ID]['descriptions']['en']['desc'],
+    ModuleSettings::CAPTURE_DELAY . Module::PAYMENT_GOOGLE_PAY_ID =>
+        Module::PAYMENT_DEFINTIONS[Module::PAYMENT_GOOGLE_PAY_ID]['descriptions']['en']['desc'],
+    ModuleSettings::CAPTURE_DELAY . Module::PAYMENT_KLARNA_IMMEDIATE_ID =>
+        Module::PAYMENT_DEFINTIONS[Module::PAYMENT_KLARNA_IMMEDIATE_ID]['descriptions']['en']['desc'],
+    ModuleSettings::CAPTURE_DELAY . Module::PAYMENT_KLARNA_LATER_ID =>
+        Module::PAYMENT_DEFINTIONS[Module::PAYMENT_KLARNA_LATER_ID]['descriptions']['en']['desc'],
+    ModuleSettings::CAPTURE_DELAY . Module::PAYMENT_KLARNA_OVER_TIME_ID =>
+        Module::PAYMENT_DEFINTIONS[Module::PAYMENT_KLARNA_OVER_TIME_ID]['descriptions']['en']['desc'],
+    ModuleSettings::CAPTURE_DELAY . Module::PAYMENT_TWINT_ID =>
+        Module::PAYMENT_DEFINTIONS[Module::PAYMENT_TWINT_ID]['descriptions']['en']['desc'],
+    ModuleSettings::CAPTURE_DELAY . Module::PAYMENT_APPLE_PAY_ID =>
+        Module::PAYMENT_DEFINTIONS[Module::PAYMENT_APPLE_PAY_ID]['descriptions']['en']['desc']
+];
+
+foreach ($paymentLang as $key => $description) {
+    $key = 'SHOP_MODULE_' . $key;
+    $paymentLang = [
+        $key => $description,
+        $key . '_' . Module::ADYEN_CAPTURE_DELAY_MANUAL => 'Manual',
+        $key . '_' . Module::ADYEN_CAPTURE_DELAY_DAYS => 'n Days',
+        $key . '_' . Module::ADYEN_CAPTURE_DELAY_IMMEDIATE => 'Immediate',
+        'HELP_' . $key => 'In Adyen you can define the delay of the capture for'
+            . $description . ': "Immediate", "after n days" or "Manual".'
+            . ' The Adyen setting must correspond to that of the shop. In the "Manual" case'
+            . ', under Manage Orders > Orders > Tab Adyen, the capture can be initiated.'
+    ];
+    $aLang += $paymentLang;
+}
+
