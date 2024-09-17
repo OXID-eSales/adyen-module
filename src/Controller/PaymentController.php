80ba4a14ea3770acd6b3cc6add36bbf1a6413c76
nils.baczynski@fatchip.de <IbJbFc21-8>
2024-09-17 14:58:18 +0200

    AM-132 validate payment in ordercontroller for adyen acdc

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -14,6 +14,7 @@ use OxidSolutionCatalysts\Adyen\Core\Module;
 use OxidSolutionCatalysts\Adyen\Model\Payment as AdyenPayment;
 use OxidSolutionCatalysts\Adyen\Service\CountryRepository;
 use OxidSolutionCatalysts\Adyen\Service\PaymentCancel;
+use OxidSolutionCatalysts\Adyen\Service\PaymentConfigService;
 use OxidSolutionCatalysts\Adyen\Service\SessionSettings;
 use OxidSolutionCatalysts\Adyen\Traits\RequestGetter;
 use OxidSolutionCatalysts\Adyen\Service\ModuleSettings;
@@ -133,15 +134,19 @@ class PaymentController extends PaymentController_parent
     public function validatePayment()
     {
         $session = $this->getServiceFromContainer(SessionSettings::class);
-        $moduleService = $this->getServiceFromContainer(ModuleService::class);
-        $actualPaymentId = $session->getPaymentId();
-        $newPaymentId = $this->getStringRequestData('paymentid');
-        $pspReference = $session->getPspReference();
+        $moduleService      = $this->getServiceFromContainer(ModuleService::class);
+        $actualPaymentId    = $session->getPaymentId();
+        $newPaymentId       = $this->getStringRequestData('paymentid');
+        $pspReference       = $session->getPspReference();
+
+        //if the payment is adyen credit card, it will be validated from the order step where a new paymentId is impossible
+        $isAdyenCreditCard  = $this->getAdyenPaymentConfigService()->isAdyenCreditCardPayment($actualPaymentId);
+        $paymentChanged     = !$isAdyenCreditCard && ($actualPaymentId !== $newPaymentId);
 
         // remove a possible old adyen payment if another one was selected
         if (
             $actualPaymentId &&
-            $actualPaymentId !== $newPaymentId &&
+            $paymentChanged &&
             $moduleService->isAdyenPayment($actualPaymentId)
         ) {
             $this->removeAdyenPaymentFromSession();
@@ -207,5 +212,10 @@ class PaymentController extends PaymentController_parent
             );
             $session->deletePaymentSession();
         }
+    }
+
+    protected function getAdyenPaymentConfigService(): PaymentConfigService
+    {
+        return $this->getServiceFromContainer(PaymentConfigService::class);
     }
 }
